dd3478de072f102b1a490aca01ae01505b658bae
Paulo Júnior <paulojrcosta300@gmail.com>
2022-11-08 18:47:12 -0300

    criando conexão com o banco de dados

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+
+
+function connect() {
+    mongoose.connect('mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+    
+    const db = mongoose.connection
+    
+    db.once('open', () => {
+        console.log('Connected to database!')
+    })
+    
+    db.on('error', console.error.bind(console, 'connection error: '))
+}
+
+module.exports = {
+    connect,
+}
