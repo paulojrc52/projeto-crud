f445e600958e4f9b94f566711d31ca564fd5511a
Paulo Júnior <paulojrcosta300@gmail.com>
2022-11-08 10:47:16 -0300

    adicionando modulos necessarios e adicionando estrutura inicial de pastas

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// Definindo o template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Definindo arquivos públicos
+app.use(express.static(path.join(__dirname, "public")))
+
+// Habilitando server para receber dados via post (formulário)
+app.use(express.urlencoded({ extended: true }))
+
+// Rotas
+app.get('/',(req, res) => {
+    res.render('index', {
+        title: 'Titulo Teste'
+    })
+})
+
+
+// 404 Error (not found)
+app.use((req, res) => { // middleware
+    res.send('Página não encontrada!')
+})
+
+// Executando Servidor  
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
