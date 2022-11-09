2f29d11b4ff953fb48db4f6424a85d857bb8cac1
Paulo Júnior <paulojrcosta300@gmail.com>
2022-11-09 10:56:45 -0300

    criando collections do banco de dados

---
+++ src/models/customers.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    name: String, 
+    age: Number,
+    email: String,
+    password: String,
+
+})
+
+const Model = mongoose.model('custoners', schema)
+
+
+module.exports = Model 
