ae97c6b57a9f380745525a2b52f349708aa8e46f
Paulo Júnior <paulojrcosta300@gmail.com>
2022-11-09 21:15:34 -0300

    criptografando senha antes de enviar pro model

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,6 +1,7 @@
 const CustormersModel = require('../models/customers')
+const { crypto } = require('../utils/password')
 
-function add(req, res) {
+async function add(req, res) {
     const {
         name,
         age,
@@ -8,11 +9,13 @@ function add(req, res) {
         password,
     } = req.body
 
+    const passwordCrypto = await crypto(password)
+
     const register = new CustormersModel ({
         name,
         age,
         email,
-        password,
+        password: passwordCrypto,
     })
 
     register.save()
