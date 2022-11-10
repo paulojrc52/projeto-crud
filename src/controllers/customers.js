9a9ebdfca139297e24ef6e128ba394a9e7f2827e
Paulo Júnior <paulojrcosta300@gmail.com>
2022-11-10 09:51:34 -0300

    criando controller para register

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,6 +1,14 @@
 const CustormersModel = require('../models/customers')
 const { crypto } = require('../utils/password')
 
+const defaultTitle = 'Cadastro de Clientes'
+
+function index(req, res) {
+    res.render('register', {
+        title: defaultTitle,
+    })
+}
+
 async function add(req, res) {
     const {
         name,
@@ -20,9 +28,25 @@ async function add(req, res) {
 
     register.save()
 
-    res.send('Cadastro realizado!')
+    res.render('register',{
+        title: defaultTitle,
+        message: 'Cadastro realizado com sucesso!'
+    }
+    )
+}
+
+
+async function listUsers(req, res) {
+    const users = await CustormersModel.find()
+
+    res.render('listUsers', {
+        title: 'Listagem de usuários',
+        users,
+    })
 }
 
 module.exports = {
-    add
+    add,
+    index,
+    listUsers, 
 }
