dfc5f5bbc826d784f6e1b32435732a25aac0beab
Paulo Júnior <paulojrcosta300@gmail.com>
2022-11-09 21:17:46 -0300

    criando controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,24 @@
 const router = require('express').Router()
 
+const CustomersController = require('../controllers/customers')
 
 
 // Rotas
 router.get('/',(req, res) => {
     res.render('index', {
-        title: 'Titulo Teste'
+        title: 'Página Inicial'
+    }) 
+})
+
+router.get('/register',(req, res) => {
+    res.render('register', {
+        title: 'Cadastro de Clientes'
     })
 })
+
+// CONTROLLER
+router.post('/register/add', CustomersController.add)
+
 
 
 module.exports = router
